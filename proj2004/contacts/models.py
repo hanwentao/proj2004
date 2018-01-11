c290bf1021efa8c52cd37d652b3b14087541932d
Wentao Han <wentao.han@gmail.com>
2018-01-11 12:39:48 +0800

    Add profile model

--- proj2004/contacts/models.py
+++ proj2004/contacts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    idnum = models.CharField('学号', max_length=10)
+    name = models.CharField('姓名', max_length=100)
+    enroll_year = models.IntegerField('入学年份')
+    graduate_year = models.IntegerField('毕业年份')
+    department = models.CharField('院系', max_length=100)
+    major = models.CharField('专业', max_length=100)
+    # class is a keyword in Python
+    clazz = models.CharField('班级', max_length=100)
+    industry = models.CharField('所在行业', max_length=100, blank=True)
+    organization = models.CharField('工作单位', max_length=100, blank=True)
+    position = models.CharField('职务', max_length=100, blank=True)
+    title = models.CharField('职称', max_length=100, blank=True)
+    mobile = models.CharField('手机', max_length=100, blank=True)
+    # Field email is already in model User
+    wechat = models.CharField('微信号', max_length=100, blank=True)
+    telephone = models.CharField('固定电话', max_length=100, blank=True)
+    location = models.CharField('所在地区', max_length=100, blank=True)
+    address = models.CharField('通讯地址', max_length=100, blank=True)
+    postcode = models.CharField('邮编', max_length=100, blank=True)
+
+    def __str__(self):
+        return self.idnum + ' ' + self.name
+
+    class Meta:
+        verbose_name = '个人信息'
+        verbose_name_plural = '个人信息'
+        ordering = ['idnum']
