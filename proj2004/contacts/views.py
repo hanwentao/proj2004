0921acff698b3e13d5bb39c842d30c056e8761f1
Wentao Han <wentao.han@gmail.com>
2018-01-23 16:05:31 +0800

    Admin can view and change all profiles

--- proj2004/contacts/views.py
+++ proj2004/contacts/views.py
@@ -25,16 +25,17 @@ def profile(request, username):
     user = get_object_or_404(User, username=username)
     password_set = user.has_usable_password()
     profile = user.profile
-    if password_set:
-        if not request.user.is_authenticated:
-            return redirect(f'{settings.LOGIN_URL}?next={request.path}')
-        if username != request.user.username:
-            print(username, repr(request.user.username))
-            return HttpResponseForbidden('你不能访问其他人的个人信息。')
-    else:
-        code = request.GET.get('code', '')
-        if code != profile.verification_code:
-            return HttpResponseForbidden('验证码错误，请与贵班联系人确认网址信息。')
+    if not request.user.is_superuser:
+        if password_set:
+            if not request.user.is_authenticated:
+                return redirect(f'{settings.LOGIN_URL}?next={request.path}')
+            if username != request.user.username:
+                print(username, repr(request.user.username))
+                return HttpResponseForbidden('你不能访问其他人的个人信息。')
+        else:
+            code = request.GET.get('code', '')
+            if code != profile.verification_code:
+                return HttpResponseForbidden('验证码错误，请与贵班联系人确认网址信息。')
     extra = user.extra
     profile_form = None
     extra_form = None
@@ -42,13 +43,13 @@ def profile(request, username):
     if request.method == 'POST':
         profile_form = ProfileForm(request.POST, instance=profile)
         extra_form = ExtraForm(request.POST, request.FILES, instance=extra)
-        if not password_set:
+        if not request.user.is_superuser and not password_set:
             set_password_form = SetPasswordForm(user, request.POST)
         if (profile_form.is_valid() and extra_form.is_valid() and
-            (password_set or set_password_form.is_valid())):
+            (request.user.is_superuser or password_set or set_password_form.is_valid())):
             profile_form.save()
             extra_form.save()
-            if not password_set:
+            if not request.user.is_superuser and not password_set:
                 set_password_form.save()
                 login(request, user)
             return redirect(profile)
@@ -56,11 +57,10 @@ def profile(request, username):
         profile_form = ProfileForm(instance=profile)
     if extra_form is None:
         extra_form = ExtraForm(instance=extra)
-    if set_password_form is None:
+    if not request.user.is_superuser and set_password_form is None:
         set_password_form = SetPasswordForm(user)
     context = {
         'page': 'profile',
-        'user': user,
         'profile': profile,
         'extra': extra,
         'profile_form': profile_form,
