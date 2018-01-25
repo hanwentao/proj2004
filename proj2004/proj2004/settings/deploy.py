959b66642860be9d7f29c508639640e8f6a4d91d
Wentao Han <wentao.han@gmail.com>
2018-01-26 00:09:04 +0800

    Check existence of settings.yml

--- proj2004/proj2004/settings/deploy.py
+++ proj2004/proj2004/settings/deploy.py
@@ -2,16 +2,19 @@ import yaml
 
 from . import *
 
-CONF_DIR = os.path.join(os.path.dirname(BASE_DIR), 'conf')
-with open(os.path.join(CONF_DIR, 'settings.yml')) as config_file:
-    config = yaml.load(config_file)
-
 DEBUG = False
-SECRET_KEY = config['secret_key']
-ALLOWED_HOSTS = config['allowed_hosts']
-DATABASES = {
-    'default': config['db'],
-}
 WWW_ROOT = '/var/www'
 STATIC_ROOT = os.path.join(WWW_ROOT, 'static')
 MEDIA_ROOT = os.path.join(WWW_ROOT, 'media')
+
+CONF_DIR = os.path.join(os.path.dirname(BASE_DIR), 'conf')
+try:
+    with open(os.path.join(CONF_DIR, 'settings.yml')) as config_file:
+        config = yaml.load(config_file)
+    SECRET_KEY = config['secret_key']
+    ALLOWED_HOSTS = config['allowed_hosts']
+    DATABASES = {
+        'default': config['db'],
+    }
+except FileNotFoundError:
+    pass
