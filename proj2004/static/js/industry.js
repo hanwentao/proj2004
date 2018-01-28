2fca0ccfd8d1656142d80cbad605b7d00844fd7b
Wentao Han <wentao.han@gmail.com>
2018-01-29 00:59:52 +0800

    Refactor industry widget

--- proj2004/static/js/industry.js
+++ proj2004/static/js/industry.js
@@ -1,28 +1,46 @@
 $(function() {
-  var updateState = function() {
-    var selectField = $("#id_industry_0");
-    var specifyField = $("#id_industry_1");
-    var value = selectField.val();
-    if (value == "其他") {
-      specifyField.show();
-    } else {
-      specifyField.hide();
-    }
-  };
-  var selectField = $("#id_industry_0");
-  var specifyField = $("#id_industry_1");
-  var value = selectField.val();
-  selectField.replaceWith(`<select name="industry_0" class="form-control" title id="id_industry_0"></select>`);
-  selectField = $("#id_industry_0");
-  selectField.append("<option value>---------</option>");
-  for (var choice of industryChoices) {
-    var option = $(`<option value="${choice}">${choice}</option>`).appendTo(selectField);
-    if (choice == value) {
-      option.attr("selected", "selected");
+  function createOptions(field, choices) {
+    field.append("<option value>---------</option>");
+    for (var choice of choices) {
+      $(`<option value="${choice}">${choice}</option>`).appendTo(field);
     }
   }
-  updateState();
+
+  function updateSpecify(masterField, slaveField) {
+    var masterValue = masterField.val();
+    if (masterValue == "其他" || masterValue == "Other") {
+      slaveField.show();
+    } else {
+      slaveField.hide();
+    }
+  }
+
+  function initState(fields, values) {
+    fields[0].val(values[0]);
+    updateSpecify(fields[0], fields[1]);
+  }
+
+  function updateState(fields, oldValues) {
+    var newValues = [fields[0].val(), fields[1].val()];
+    if (newValues[0] != oldValues[0]) {
+      fields[1].val("");
+      updateSpecify(fields[0], fields[1]);
+    }
+    oldValues[0] = newValues[0];
+    oldValues[1] = newValues[1];
+  }
+
+  var name = "industry";
+  var id = `id_${name}`;
+  var selectField = $(`#${id}_0`);
+  var specifyField = $(`#${id}_1`);
+  var values = [selectField.val(), specifyField.val()];
+  selectField.replaceWith(`<select name="${name}_0" class="form-control" title id="${id}_0"></select>`);
+  selectField = $(`#${id}_0`);
+  var fields = [selectField, specifyField];
+  createOptions(selectField, industryChoices);
   selectField.change(function() {
-    updateState();
+    updateState(fields, values);
   });
+  initState(fields, values);
 });
