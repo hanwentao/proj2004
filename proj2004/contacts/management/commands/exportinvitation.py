15d0bb5d6cc5b88a8d45b40605357340f844b45f
Wentao Han <wentao.han@gmail.com>
2018-01-31 20:47:00 +0800

    Export by department or by class

--- proj2004/contacts/management/commands/exportinvitation.py
+++ proj2004/contacts/management/commands/exportinvitation.py
@@ -18,49 +18,52 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         prefix = options['prefix']
-        if not prefix:
-            raise CommandError('Must specify prefix')
         by_department = options['department']
         by_class = options['class']
         if not by_department and not by_class:
             raise CommandError('Must specify either by deparment or by class')
         if by_department and by_class:
             raise CommandError('Cannot specify both by department and by class')
-        if by_department:
-            with connection.cursor() as cursor:
+        with connection.cursor() as cursor:
+            if by_department:
                 cursor.execute('SELECT DISTINCT department FROM contacts_profile')
-                departments = cursor.fetchall()
-            departments = [d[0] for d in departments]
-            for department in departments:
-                wb = Workbook()
-                ws = wb.active
-                ws.title = '名单'
-                ws.append([
-                    '学号',
-                    '姓名',
-                    '院系',
-                    '班级',
-                    '邀请链接',
-                ])
-                ws.column_dimensions[get_column_letter(1)].width = 15
-                ws.column_dimensions[get_column_letter(2)].width = 15
-                ws.column_dimensions[get_column_letter(3)].width = 20
-                ws.column_dimensions[get_column_letter(4)].width = 10
-                ws.column_dimensions[get_column_letter(5)].width = 60
-                alumni = Profile.objects.filter(department=department)
-                for i, alumnus in enumerate(alumni):
-                    ws.append([
-                        alumnus.student_id,
-                        alumnus.name,
-                        alumnus.department,
-                        alumnus.clazz,
-                        settings.BASE_URL + alumnus.get_absolute_url() + 'edit/?code=' + alumnus.verification_code,
-                    ])
-                path = f'{prefix}-{department}.xlsx'
-                wb.save(path)
-        else:  # by_class
-            # TODO: Complete this
-            with connection.cursor() as cursor:
+            else:
                 cursor.execute('SELECT DISTINCT clazz FROM contacts_profile')
-                classes = cursor.fetchall()
-                classes = [c[0] for c in classes]
+            groups = cursor.fetchall()
+        groups = [g[0] for g in groups]
+        prefix = '' if prefix is None else prefix
+        for group in groups:
+            name = '空' if not group else group
+            if not prefix or prefix.endswith('/'):
+                path = f'{prefix}{name}.xlsx'
+            else:
+                path = f'{prefix}-{name}.xlsx'
+            print(f'Generating {path}...')
+            wb = Workbook()
+            ws = wb.active
+            ws.title = '名单'
+            ws.append([
+                '学号',
+                '姓名',
+                '院系',
+                '班级',
+                '邀请链接',
+            ])
+            ws.column_dimensions[get_column_letter(1)].width = 15
+            ws.column_dimensions[get_column_letter(2)].width = 15
+            ws.column_dimensions[get_column_letter(3)].width = 20
+            ws.column_dimensions[get_column_letter(4)].width = 10
+            ws.column_dimensions[get_column_letter(5)].width = 60
+            if by_department:
+                alumni = Profile.objects.filter(department=group)
+            else:
+                alumni = Profile.objects.filter(clazz=group)
+            for i, alumnus in enumerate(alumni):
+                ws.append([
+                    alumnus.student_id,
+                    alumnus.name,
+                    alumnus.department,
+                    alumnus.clazz,
+                    settings.BASE_URL + alumnus.get_absolute_url() + 'edit/?code=' + alumnus.verification_code,
+                ])
+            wb.save(path)
