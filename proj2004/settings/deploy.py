8543f3826d13dd4f9e320fab716a6dc5c96397c7
Wentao Han <wentao.han@gmail.com>
2018-02-04 04:44:50 +0800

    Change settings format and fix a path bug

--- proj2004/settings/deploy.py
+++ proj2004/settings/deploy.py
@@ -7,15 +7,12 @@ WWW_ROOT = '/var/www'
 STATIC_ROOT = os.path.join(WWW_ROOT, 'static')
 MEDIA_ROOT = os.path.join(WWW_ROOT, 'media')
 
-CONF_DIR = os.path.join(os.path.dirname(BASE_DIR), 'conf')
+CONF_DIR = os.path.join(BASE_DIR, 'conf')
 try:
     with open(os.path.join(CONF_DIR, 'settings.yml')) as config_file:
         config = yaml.load(config_file)
-    BASE_URL = config['base_url']
-    SECRET_KEY = config['secret_key']
-    ALLOWED_HOSTS = config['allowed_hosts']
-    DATABASES = {
-        'default': config['db'],
-    }
+    globals().update(config)
+    del config, config_file
 except FileNotFoundError:
     pass
+del yaml
