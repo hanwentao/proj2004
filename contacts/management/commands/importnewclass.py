e00a69457f5b6a224b5de337cbaa679c0fda2084
Wentao Han <wentao.han@gmail.com>
2018-02-14 23:56:24 +0800

    Add import new class command

---
+++ contacts/management/commands/importnewclass.py
@@ -0,0 +1,44 @@
+import argparse
+import csv
+import sys
+
+from django.core.management.base import BaseCommand
+
+from ...models import (
+    Clazz,
+    Profile,
+)
+
+
+class Command(BaseCommand):
+    help = 'Import new class information.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('newclass', nargs='*', type=argparse.FileType(encoding='utf-8'), help='New class information file.')
+
+    def handle(self, *args, **options):
+        newclass_files = options['newclass'] if options['newclass'] else [sys.stdin]
+        for newclass_file in newclass_files:
+            csv_reader = csv.reader(newclass_file)
+            for row in csv_reader:
+                name, student_id, class_list = row
+                try:
+                    if student_id:
+                        profile = Profile.objects.get(student_id=student_id)
+                    else:
+                        profile = Profile.objects.get(name=name)
+                    user = profile.user
+                except Profile.DoesNotExist:
+                    self.stderr.write(self.style.WARNING(f'{student_id} {name} does not exist.'))
+                    continue
+                except Profile.MultipleObjectsReturned:
+                    self.stderr.write(self.style.WARNING(f'{name} duplicated.'))
+                    continue
+                for class_name in class_list.split():
+                    try:
+                        class_ = Clazz.objects.get(name=class_name)
+                    except Clazz.DoesNotExist:
+                        self.stderr.write(self.style.WARNING(f'Class {class_name} does not exist.'))
+                        continue
+                    profile.clazzes.add(class_)
+                    self.stdout.write(self.style.SUCCESS(f'Added {name} to class {class_name}.'))
