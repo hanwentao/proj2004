06bee2caad53cf0d5d019fe678e7e69e2972cdd4
Wentao Han <wentao.han@gmail.com>
2018-04-06 17:35:01 +0800

    Improve profile list display

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import (
     Department,
@@ -29,13 +31,33 @@ class ClassAdmin(admin.ModelAdmin):
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = [
+    search_fields = (
         'student_id',
         'name',
         'major',
         'classes__name',
         'classes__department__name',
-    ]
+    )
+    list_display = (
+        'student_id',
+        'name',
+        'class_name',
+        'extra',
+    )
+    list_display_links = (
+        'student_id',
+        'name',
+    )
+
+    def class_name(self, obj):
+        return obj.class_name
+    class_name.short_description = '班级'
+
+    def extra(self, obj):
+        return format_html('<a href="{}">额外信息</a>',
+            reverse('admin:contacts_extra_change', args=(obj.user.extra.id,)))
+    extra.short_description = '额外信息'
+
 
 admin.site.register(Department, DepartmentAdmin)
 admin.site.register(Class, ClassAdmin)
